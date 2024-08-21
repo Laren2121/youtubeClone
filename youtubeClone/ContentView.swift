2a87d92c473bf247031c3c942173bcfd3d2ae913
Laren D'Cruz <larenmark@gmail.com>
2024-08-20 22:04:38 -0400

    Initial Commit

---
+++ youtubeClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  youtubeClone
+//
+//  Created by Laren Mark D'Cruz on 2024-08-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ youtubeClone/youtubeCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtubeCloneApp.swift
+//  youtubeClone
+//
+//  Created by Laren Mark D'Cruz on 2024-08-20.
+//
+
+import SwiftUI
+
+@main
+struct youtubeCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
